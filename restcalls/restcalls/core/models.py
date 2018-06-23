62f8f14df1d0eb1a172bfa71d8130c171f513ac6
José Cordeiro de Oliveira Junior <cordjr@gmail.com>
2018-06-23 17:37:53 -0300

    created models Subscriber and CallRecord

--- restcalls/restcalls/core/models.py
+++ restcalls/restcalls/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Subscriber(models.Model):
+    phone_number = models.IntegerField(primary_key=True)
+
+
+class CallRecord(models.Model):
+    destination_number = models.IntegerField
+    start_time = models.DateTimeField
+    end_time = models.DateTimeField
