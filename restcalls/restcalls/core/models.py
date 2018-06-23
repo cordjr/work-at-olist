b1b5bcc434ccc857c584155513230475c25c2a85
José Cordeiro de Oliveira Junior <cordjr@gmail.com>
2018-06-23 19:18:15 -0300

    pricePlice and priceRule added to model

--- restcalls/restcalls/core/models.py
+++ restcalls/restcalls/core/models.py
@@ -6,3 +6,18 @@ class CallRecord(models.Model):
     destination_number = models.IntegerField()
     start_time = models.DateTimeField(null=True)
     end_time = models.DateTimeField(null=True)
+
+
+class PricePolice(models.Model):
+    start = models.DateField
+    end = models.DateField
+
+
+class PriceRule(models.Model):
+    TYPES = (('S', 'STANDARD'),
+             ('R', 'REDUCED'))
+    policy = models.ForeignKey(PricePolice, on_delete=models.CASCADE)
+    start_time = models.TimeField
+    end_time = models.TimeField
+    type = models.CharField(max_length=1, choices=TYPES)
+    value = models.DecimalField(max_digits=20, decimal_places=2)
