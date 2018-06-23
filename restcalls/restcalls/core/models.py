d61ea6bbbb8694315d039aa42dd07cfd29a0e0a4
José Cordeiro de Oliveira Junior <cordjr@gmail.com>
2018-06-23 18:46:45 -0300

    model refactoring removed subscriber and its relationship

--- restcalls/restcalls/core/models.py
+++ restcalls/restcalls/core/models.py
@@ -1,12 +1,8 @@
 from django.db import models
 
 
-class Subscriber(models.Model):
-    phone_number = models.IntegerField(primary_key=True)
-
-
 class CallRecord(models.Model):
+    source_number = models.IntegerField(db_index=True)
     destination_number = models.IntegerField()
     start_time = models.DateTimeField(null=True)
     end_time = models.DateTimeField(null=True)
-    subscriber = models.ForeignKey(Subscriber, on_delete=models.CASCADE)
