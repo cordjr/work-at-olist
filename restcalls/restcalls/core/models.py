a7af2a5df82215309881a25a681a47dfcf0094dc
José Cordeiro de Oliveira Junior <cordjr@gmail.com>
2018-06-23 22:46:04 -0300

    added tests to CallRecord and PricePolicy models also created some migration to fix errors

--- restcalls/restcalls/core/models.py
+++ restcalls/restcalls/core/models.py
@@ -2,15 +2,26 @@ from django.db import models
 
 
 class CallRecord(models.Model):
+    call_id = models.IntegerField(primary_key=True)
     source_number = models.IntegerField(db_index=True)
     destination_number = models.IntegerField()
     start_time = models.DateTimeField(null=True)
     end_time = models.DateTimeField(null=True)
+    minute_price = models.DecimalField(max_digits=20, decimal_places=2, null=True)
+
+    @property
+    def total_price(self):
+        return self.total_minutes * self.minute_price
+
+    @property
+    def total_minutes(self):
+        delta = (self.end_time - self.start_time)
+        return delta.seconds // 60
 
 
 class PricePolice(models.Model):
-    start = models.DateField
-    end = models.DateField
+    start = models.DateField(default=None)
+    end = models.DateField(default=None)
 
 
 class PriceRule(models.Model):
